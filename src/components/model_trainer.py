ba5f8fd0b154e5075a42c27fa306dc1bcc5cb502
Sriman Chaudhuri <sriman@Mac.Dlink>
2026-01-28 14:56:43 +0530

    Hyper-parameter Tuning done

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,7 +1,6 @@
 import os, sys
 from pydantic import BaseModel, Field
 
-from catboost import CatBoostRegressor
 from sklearn.ensemble import (
     AdaBoostRegressor,
     GradientBoostingRegressor,
@@ -37,14 +36,38 @@ class ModelTrainer:
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
                 "XGB Regressor": XGBRegressor(),
-                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor()
                 }
+            
+            params = {
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                },
+                "Random Forest": {
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Gradient Boosting": {
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Linear Regression": {},
+                "XGB Regressor": {
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "AdaBoost Regressor": {
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                }
+            }
 
-            model_report: dict = evaluate_models(X_train, y_train, X_test, y_test, models)
+            model_report: dict = evaluate_models(X_train, y_train, X_test, y_test, models, params)
 
-            best_model_name = max(model_report, key=model_report.get)
-            best_model_score = model_report[best_model_name]
+            best_model_score = max(model_report.values())
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
             best_model = models[best_model_name]
 
             if best_model_score < 0.6:
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,7 @@
 import os, sys, dill
 
-import numpy as np
-import pandas as pd
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import ExceptionHandler
 
@@ -16,12 +15,18 @@ def save_object(file_path, obj):
     except Exception as e:
         ExceptionHandler(e, sys)
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params) -> dict:
     try:
         report = {}
 
         for i in range(len(models)):
             model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, param, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
 
             y_test_pred = model.predict(X_test)
@@ -33,4 +38,4 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
         return report
 
     except Exception as e:
-        ExceptionHandler(e, sys)
+        raise ExceptionHandler(e, sys)
